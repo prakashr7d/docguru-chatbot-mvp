eb506a6f6026f4f8f8e960d029eed6a369e225ab
horizon733 <dishantgandhi733@gmail.com>
2021-04-21 12:41:38 +0530

    AB#1002 added cli script with help of click

--- src/dash_ecomm/elastic_search_data_upload.py
+++ src/dash_ecomm/elastic_search_data_upload.py
@@ -1,25 +1,49 @@
+import click
+from dash_ecomm.constants import (
+    E_COMM,
+    ELASTICSEARCH_URL,
+    PRODUCT_EXCEL_SHEET,
+    PRODUCTS_JSON,
+)
 from dash_ecomm.database_utils import get_products_to_json, upload_data_to_elastic
 from elasticsearch import Elasticsearch
 
-get_products_to_json("e-comm-products.xlsx")
-upload_data_to_elastic("products.json")
-es = Elasticsearch("http://localhost:9200")
-query = {
-    "_source": [],
-    "size": 5,
-    "query": {
-        "bool": {
-            "filter": [
-                {"multi_match": {"query": "laptops", "fields": ["sub_category"]}}
-            ]
-        }
-    },
-}
+es = Elasticsearch(ELASTICSEARCH_URL)
 
-products = es.search(index="e_comm", body=query, scroll="1m")
-print(products)
-for i in products["hits"]["hits"]:
-    print(i["_source"]["brand"])
-# products = es.search(index="e_comm", body=query)
-# for i in products:
-#     print(i)
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command(name="upload", help="uploads data onto elastic search index")
+def upload_data():
+    if es.indices.exists(index=E_COMM):
+        es.indices.delete(index=E_COMM)
+        get_products_to_json(PRODUCT_EXCEL_SHEET)
+        upload_data_to_elastic(PRODUCTS_JSON)
+    else:
+        get_products_to_json(PRODUCT_EXCEL_SHEET)
+        upload_data_to_elastic(PRODUCTS_JSON)
+
+
+@cli.command(name="show", help="shows laptops name from elastic search")
+def show_data():
+    query = {
+        "_source": [],
+        "size": 5,
+        "query": {
+            "bool": {
+                "filter": [
+                    {"multi_match": {"query": "laptops", "fields": ["sub_category"]}}
+                ]
+            }
+        },
+    }
+    products = es.search(index="e_comm", body=query, scroll="1m")
+    for i in products["hits"]["hits"]:
+        print(i["_source"]["brand"])
+
+
+if __name__ == "__main__":
+    cli()
